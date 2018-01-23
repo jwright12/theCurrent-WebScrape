b5c47675b52c0e13957f772ad18178f1624b2a7b
jwright12 <jwright12@winona.edu>
2018-01-23 16:07:23 -0600

    Updated driver class to new environmentls

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from db_mgmt import DataBaseConnection
-import webScraper
+from database_manager import DataBaseConnection
+import scraper
 
-# Submit the URL for web scrape and parsing
-    webScrape = webScraper.webScrapeTheCurrent()
+URL = 'https://www.thecurrent.org/playlist'
+# Scrape todays playlist by running at 11:59pm at URL below
+webScrape = scraper.webScrapeTheCurrent(URL)
 
-    # Call method that cleans the web scrape and packs it in a list of dictionaries
-    song_dictionaries = webScrape.musicHTMLParser()
+# Call method that cleans the web scrape and packs it in a list of dictionaries
+song_dictionaries = webScrape.musicHTMLParser()
 
-    # Instantiate DB connection
-    db_connect = DataBaseConnection()
+song_dictionaries
 
-    # Pass dictionary to unpack and load into DB
-    db_connect.insertDB(song_dictionaries)
+# Instantiate DB connection
+db_connect = DataBaseConnection()
 
-    # Create cursor object from DB instantiation
-    # Verify DB connection
+# Pass dictionary to unpack and load into DB
+db_connect.insertDB(song_dictionaries)
+
+# Create cursor object from DB instantiation
+# Verify DB connection
